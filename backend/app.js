03b442eaad3c439f9fe33bc37e111a7d698cecfa
ritesh455 <riteshpilavare9011@gmail.com>
2026-03-25 00:31:31 +0530

    changes in app.js

--- backend/app.js
+++ backend/app.js
@@ -66,12 +66,12 @@ app.get("/", (req, res) => {
 app.use(errorMiddleware);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // server.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
 // });
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
