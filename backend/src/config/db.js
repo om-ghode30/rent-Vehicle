121a34c689a03c3ecd9847e4684a72e28640d5e9
ritesh455 <riteshpilavare9011@gmail.com>
2026-03-25 11:51:00 +0530

    Changes in db file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -13,14 +13,30 @@
 
 // module.exports = db;
 
+// const mysql = require("mysql2/promise");
+
+// const pool = mysql.createPool({
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+//   port: process.env.MYSQL_PORT,
+// });
+
+// console.log("MySQL Connected");
+
+// module.exports = pool;
+
+
+
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 console.log("MySQL Connected");
